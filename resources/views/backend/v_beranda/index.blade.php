a17cc8275995b9cd253e636986439958d19202bf
ardiandp <ardiandp@gmail.com>
2024-12-10 09:58:39 +0700

    baru bisa login

---
+++ resources/views/backend/v_beranda/index.blade.php
@@ -0,0 +1,9 @@
+@extends('backend.v_layouts.app')
+@section('content')
+
+{{ $judul}}
+<h2>Selamat Datang  di Halaman Backend</h2>
+
+
+<!-- contentAkhir -->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])->name('backend.beranda');
+
+Route::get('backend/login', [LoginController::class, 'loginBackend'])->name('backend.login');
+Route::post('backend/login', [LoginController::class, 'authenticateBackend'])->name('backend.login');
+Route::get('backend/logout', [LoginController::class, 'logoutBackend'])->name('backend.logout');
